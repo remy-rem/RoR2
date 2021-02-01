58b91d5cbbf71108798bf5d11c2c49535a44e7a6
remy-rem <76681481+remy-rem@users.noreply.github.com>
2021-01-31 17:17:53 -0800

    Add files via upload

---
+++ Loader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace riskofrainfunsies
+{
+    public class Loader
+    {
+        // Declare our GameObject
+        public static GameObject Load;
+
+        // Our Loader Method
+        public static void Init()
+        {
+            // Create an instance of the Gameobject
+            Load = new GameObject();
+
+            // Example of adding an MonoBehaviour component to our GameObject
+            Load.AddComponent<Main>();
+
+            // Telling Unity not to destory our GameObject on level change
+            Object.DontDestroyOnLoad(Load);
+        }
+
+        public static void Unload()
+        {
+            // Destory our GameObject
+            Object.Destroy(Load);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    RoR2.CharacterMaster Player;
+    private void Start()
+    {
+        //Do stuff here once for initialization
+        Player = FindObjectOfType<RoR2.CharacterMaster>();
+    }
+    private void Update()
+    {
+        //Do stuff here on every tick
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            Player.GiveMoney(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            riskofrainfunsies.Loader.Unload();
+        }
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "fork");
+    }
+}
+
+
+
+
+
